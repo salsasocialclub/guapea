ae027477b2cd1b29a30e99bd0c3defde152174d7
Gerardo Reyes <grevych@gmail.com>
2019-07-26 11:28:10 -0500

    Add helmet middlewares to the API

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const middlewares = require('./middlewares');
 const usersAPI = require('./users');
@@ -7,14 +8,14 @@ const venuesAPI = require('./venues');
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.disable('x-powered-by')
 // app.use(middlewares.<something>)
 
 module.exports = (db) => {
 
   app.use('/v1/users', usersAPI(db, app));
   app.use('/v1/venues', venuesAPI(db, app));
-
-  app.disable('x-powered-by')
 
   return app;
 };
